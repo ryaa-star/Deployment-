72c01ad94f98b696884553d2e594fc3c55ccda31
Rupal <rupalrya@gmail.com>
2026-01-16 19:05:23 -0800

    Add p2 project code

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const colors = ["green", "red", "rgba(133,122,200)", "f15025"];
-const btn =  document.getElementById("btn");
-const color = document.querySelector(".color");
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-btn.addEventListener("click", function () {
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
 });
-
-function getRandomNumber() {
-    return Math.floor( Math.random() * colors.length);
-}
