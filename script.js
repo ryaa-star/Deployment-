d787296a713112526f29b52ce48e51aa79fa086a
Rupal <rupalrya@gmail.com>
2026-02-16 12:37:25 +0530

    new colors portfolio and features

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const typingElement = document.getElementById('typing-text');
 const textToType = "I create beautiful web experiences ";
 let charIndex = 0;
+
 function typeEffect() {
   if (charIndex < textToType.length) {
     typingElement.textContent += textToType.charAt(charIndex);
@@ -10,3 +11,10 @@ function typeEffect() {
 }
 
 typeEffect();
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelectorAll('.nav-link').forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
